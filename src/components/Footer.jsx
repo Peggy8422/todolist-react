e9649fd3b009d202df9e1f235f0bfb682872c3e1
Peggy8422 <pig2222pig222@gmail.com>
2022-11-28 22:38:29 +0800

    feat: Add column of showing unDone todo amount

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,10 +5,14 @@ const StyledFooter = styled.footer`
   justify-content: space-between;
 
   padding: 0 16px;
+  .p-box {
+    display: flex;
+  }
+
   p {
     font-size: 14px;
     font-weight: 300;
-    margin: 2rem 0 1rem;
+    margin: 2rem 1rem 1rem;
   }
 `;
 
@@ -34,7 +38,10 @@ const StyledButton = styled.button`
 const Footer = ({todos}) => {
   return (
     <StyledFooter>
-      <p>剩餘項目數： {todos.length}</p>
+      <div className='p-box'>
+        <p>剩餘項目數： {todos.length}</p>
+        <p>未完成項目數： {todos.filter((todo) => !todo.isDone).length}</p>
+      </div>
       <StyledButton>登出</StyledButton>
     </StyledFooter>
   );
