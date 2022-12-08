edb4ab07d9ec84bb26b7dac2bfbf7ed39b854749
Peggy8422 <pig2222pig222@gmail.com>
2022-12-09 00:04:14 +0800

    feat: add router basename

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { HomePage, TodoPage, LoginPage, SignUpPage } from './pages';
 import { AuthProvider } from 'contexts/AuthContext';
 
+const basename = process.env.PUBLIC_URL;
+
 function App() {
   return (
     <div className="app">
-      <BrowserRouter>
+      <BrowserRouter basename={basename}> {/*在此設定專案的首頁路由名稱*/}
         <AuthProvider>
           <Routes>
             <Route path="login" element={<LoginPage />} />
